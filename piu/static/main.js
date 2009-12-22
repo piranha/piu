1c3d2421cab3934c792fe9b5e6a20a991624ff47
Alexander Solovyov <piranha@piranha.org.ua>
2009-12-23 01:52:24 +0200

    user interface to selection ranges

--- piu/static/main.js
+++ piu/static/main.js
@@ -20,14 +20,21 @@ $(document).ready(function() {
     shortcut.add('ctrl+j', function() { lexers.focus(); });
     shortcut.add('ctrl+n', function() { document.location.href = '/'; });
 
+    $('.linenos a').each(function() {
+        this.rel = this.href.split('#')[1];
+        this.removeAttribute('href');
+    }).click(function() {
+        window.location.hash = '#' + this.rel;
+    });
+
     // highlight hovered lines
     $('.line').hover(
         function() { $(this).addClass('over'); },
         function() { $(this).removeClass('over'); }
         );
     $('.linenos a').hover(
-        function() { $('#' + this.href.split('#')[1]).addClass('over'); },
-        function() { $('#' + this.href.split('#')[1]).removeClass('over'); }
+        function() { $('#' + this.rel).addClass('over'); },
+        function() { $('#' + this.rel).removeClass('over'); }
         );
 
     hlter.run();
@@ -35,6 +42,8 @@ $(document).ready(function() {
 
 hlter = {
     last_location: null,
+    // if user is selecting lines right now, first element
+    selecting: null,
     // time in ms that the URL is queried for changes
     run_interval_every: 50,
 
@@ -44,6 +53,20 @@ hlter = {
         this._interval = setInterval(function() {
             app.check.apply(app);
         }, this.run_interval_every);
+
+        $('.linenos a').mousedown(function() {
+            app.selecting = this.rel;
+        }).mouseup(function() {
+            window.location.hash = '#' + app.selecting + ':' + this.rel;
+            app.selecting = null;
+
+            if (window.getSelection) {
+                window.getSelection().removeAllRanges();
+            } else {
+                // no idea how to clear it
+                document.selection.createRange();
+            }
+        });
     },
 
     perform: function() {
@@ -53,7 +76,10 @@ hlter = {
             var pair = specifiers[k].split(':');
             if (!(pair.length == 2))
                 pair[1] = pair[0];
-            for (i = this.int(pair[0]); i <= this.int(pair[1]); i += 1) {
+            start = this.int(pair[0]); end = this.int(pair[1]);
+            if (start > end)
+                end = (start += end -= start) - end; // swap variables
+            for (i = start; i <= end; i += 1) {
                 $('#l-' + i).addClass('selected');
             }
         }

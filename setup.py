15f3f1d22d2ef76213d508a20e65cf47a733a587
Alexander Solovyov <alexander@solovyov.net>
2011-02-06 16:50:09 +0100

    forgot about jinja2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='piu',
       author_email='piranha@piranha.org.ua',
       packages=['piu'],
       include_package_data=True,
-      install_requires=['opster', 'bottle', 'redis', 'pygments', 'CherryPy'],
+      install_requires=['opster', 'bottle', 'redis', 'pygments', 'CherryPy', 'jinja2'],
       entry_points='''
       [console_scripts]
       piud = piu:main

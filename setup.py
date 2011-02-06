ff070dd72928043e0924cf0a90e47614118a0316
Alexander Solovyov <alexander@solovyov.net>
2011-02-06 16:40:45 +0100

    pygments required

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='piu',
       author_email='piranha@piranha.org.ua',
       packages=['piu'],
       include_package_data=True,
-      install_requires=['opster', 'bottle', 'redis'],
+      install_requires=['opster', 'bottle', 'redis', 'pygments'],
       entry_points='''
       [console_scripts]
       piud = piu:main

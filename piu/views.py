10996a40cfa1b3952bd067f5b0c655f65a2b8e40
Alexander Solovyov <alexander@solovyov.net>
2015-07-05 12:47:11 +0300

    getting ready for the public release

--- piu/views.py
+++ piu/views.py
@@ -1,6 +1,7 @@
+import os
+import os.path as op
 import json
 import re
-import os.path as op
 from hashlib import sha1
 from datetime import datetime as dt
 
@@ -13,21 +14,21 @@ from piu.utils import highlight, style, lexerlist, dec
 from piu.utils import toepoch, fromepoch
 
 
-cookie = {'max_age': 60*60*24*30*12, 'path': '/'}
-spamre = re.compile('^comment\d+,')
-secret = 'epic-secret'
+COOKIE = {'max_age': 60*60*24*30*12, 'path': '/'}
+SPAMRE = re.compile('^comment\d+,')
+SECRET = os.environ.get('SECRET', 'epic-secret')
 
 
 def sign(id, data):
     if isinstance(data, unicode):
         data = data.encode('utf-8')
-    return sha1(secret + str(id) + data).hexdigest()
+    return sha1(SECRET + str(id) + data).hexdigest()
 
 
 def paste(item, data, lexer):
     '''actually store data'''
-    response.set_cookie('lexer', lexer, **cookie)
-    response.set_cookie('edit-%s' % item.id, item.id, secret=secret, **cookie)
+    response.set_cookie('lexer', lexer, **COOKIE)
+    response.set_cookie('edit-%s' % item.id, item.id, secret=SECRET, **COOKIE)
 
     item['raw'] = data
     result, lexer = highlight(data, lexer)
@@ -35,6 +36,7 @@ def paste(item, data, lexer):
     item['html'] = result
     item['date'] = toepoch(dt.now())
     item.save()
+
 
 def regenerate():
     for k in store:
@@ -64,7 +66,7 @@ def new():
     if not data:
         return redirect('/', 302)
 
-    if spamre.search(data):
+    if SPAMRE.search(data):
         abort(402, 'Wanna spam? Pay me money! ;)')
 
     lexer = request.POST.get('lexer', 'guess')
@@ -81,12 +83,11 @@ def redirect_show(id):
 @route('/:id/')
 def show(id):
     try:
-        id = int(id)
-        item = store[id]
+        item = store[int(id)]
     except (ValueError, KeyError):
         return redirect('/', 302)
 
-    owner = bool(request.get_cookie('edit-%s' % id, '', secret=secret))
+    owner = bool(request.get_cookie('edit-%s' % id, '', secret=SECRET))
 
     lexername = request.GET.get('as') or item['lexer']
     lexer = lexers.get_lexer_by_name(lexername)
@@ -103,11 +104,11 @@ def show(id):
     return template('show', item=item, owner=owner, lexer=lexer,
                     lexers=lexerlist(), date=fromepoch(item.get('date', 0)))
 
+
 @route('/:id/raw/')
 def show_raw(id):
     try:
-        id = int(id)
-        item = store[id]
+        item = store[int(id)]
     except (ValueError, KeyError):
         return redirect('/', 302)
 
@@ -115,16 +116,16 @@ def show_raw(id):
     response.content_type = '%s; charset=utf-8' % ctype
     return item['raw'].encode('utf-8')
 
+
 @route('/:id/edit/')
 @route('/:id/edit/', method='POST')
 def edit(id):
     try:
-        id = int(id)
-        item = store[id]
+        item = store[int(id)]
     except (ValueError, KeyError):
         return redirect('/', 302)
 
-    owner = bool(request.get_cookie('edit-%s' % id, secret=secret))
+    owner = bool(request.get_cookie('edit-%s' % id, secret=SECRET))
     if not owner:
         return redirect('/%s/' % id, 302)
 
@@ -137,6 +138,7 @@ def edit(id):
     return template('index', data=item['raw'], id=id,
                     deflexer=item['lexer'], lexers=lexerlist())
 
+
 @route('/piu')
 def piu():
     lexdict = {}
@@ -148,10 +150,12 @@ def piu():
     response.content_type = 'text/plain'
     return template('piu.py', extmap=lexdict, lexers=lexlist)
 
+
 @route('/piu.el')
 def piuel():
     response.content_type = 'text/plain'
     return template('piu.el')
+
 
 @route('/about/')
 def about():

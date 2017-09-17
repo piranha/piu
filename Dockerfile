c25b35ae3c4acb0e1122524085d9856146c2cab8
Alexander Solovyov <alexander@solovyov.net>
2017-09-17 10:40:56 +0300

    speed up docker image

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM alpine
+FROM alpine:3.6
 
-RUN apk add --no-cache curl tar xz ca-certificates gcc python python-dev py-setuptools musl-dev && update-ca-certificates
+RUN apk add --no-cache gcc musl-dev python2 python2-dev \
+    py2-pip py2-pygments py2-bottle py2-tornado py2-jinja2
 
 RUN mkdir /app
 WORKDIR /app
-COPY . /app/
+COPY requirements.txt /app
+RUN pip install -r /app/requirements.txt
+
 VOLUME ["/store"]
 EXPOSE 80
-
-RUN python setup.py install
+COPY . /app/
+RUN python setup.py --quiet install
 
 CMD ["piud", "-a", "0.0.0.0", "-p", "80", "--path", "/store"]
--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ from setuptools import setup
 if 'register' in sys.argv:
     raise Exception('I don\'t want to be on PyPI!')
 
+reqs = [x for x in
+        open('requirements.txt').read().strip().splitlines()
+        if not x.startswith('#')]
+
 setup(name='piu',
       description='paste.in.ua',
       version='0.2',
@@ -14,12 +18,7 @@ setup(name='piu',
       license='ISC',
       packages=['piu'],
       include_package_data=True,
-      install_requires=['opster==4.1',
-                        'bottle==0.12.9',
-                        'Pygments==2.1',
-                        'tornado==4.3',
-                        'jinja2==2.8',
-                        'tnetstring==0.2.1'],
+      install_requires=reqs,
       entry_points='''
       [console_scripts]
       piud = piu:main.command
